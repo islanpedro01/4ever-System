d8e11c38df7f138ad2cd989425c70e25ac51f62b
Gabryel Araújo <gabryel.dias@academico.ifpb.edu.br>
2023-12-02 20:45:33 -0300

    Ajustes
    Foram feitos ajustes na subClasse Convivado, inserindo o atributo String empresa.
    Ademais, foram feitos todos os métodos da classe Fachada conforme solicitado no documento de requisitos.
    Obs.: os métodos estão apenas implementados de maneira crua, sem verificação de integridade e nem tratamento de exceção.

--- src/regras_negocio/Fachada.java
+++ src/regras_negocio/Fachada.java
@@ -2,8 +2,11 @@ package regras_negocio;
 
 import modelo.Convidado;
 import modelo.Evento;
+import modelo.Ingresso;
 import modelo.Participante;
 import repositorio.Repositorio;
+
+import java.util.ArrayList;
 
 public class Fachada {
     private static Repositorio repositorio = new Repositorio();
@@ -26,7 +29,7 @@ public class Fachada {
         repositorio.adicionar(participante);
     }
 
-    public static void criarConvidado(String cpf, String nascimento, String empresa) throws Exception{
+    public static void criarConvidado(String cpf, String nascimento, String empresa) throws Exception {
         Participante part = repositorio.localizarParticipante(cpf);
 
         if (part != null) {
@@ -34,10 +37,41 @@ public class Fachada {
             throw new Exception(mensagem);
         }
 
-        Convidado convidado = new Convidado(cpf, nascimento);
-        convidado.setEmpresa(empresa);
+        Convidado convidado = new Convidado(cpf, nascimento, empresa);
         repositorio.adicionarConvidado(convidado);
     }
 
-    public static void criarIngresso
+    public static void criarIngresso(int id, String cpf, String telefone) {
+        Evento e = repositorio.localizarEvento(id);
+        String codigo = id + " - " + cpf;
+
+        Ingresso ingresso = new Ingresso(codigo, e, telefone);
+    }
+
+    public static void apagarEvento(int id){
+        Evento e = repositorio.localizarEvento(id);
+        repositorio.remover(e);
+    }
+
+    public static void apagarParticipante(String cpf){
+        Participante p = repositorio.localizarParticipante(cpf);
+        repositorio.remover(p);
+    }
+
+    public static void apagarIngresso(String codigo){
+        Ingresso i = repositorio.localizarIngresso(codigo);
+        repositorio.remover(i);
+    }
+
+    public static ArrayList<Evento> listarEventos(){
+        return repositorio.getEventos();
+    }
+
+    public static ArrayList<Participante> listarParticipantes(){
+        return repositorio.getParticipantes();
+    }
+
+    public static  ArrayList<Ingresso> listarIngressos(){
+        return repositorio.getIngressos();
+    }
 }
